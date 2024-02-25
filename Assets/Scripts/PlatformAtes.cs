c29dc5e3d0033234418d71fd78df3e2bb2796356
Sezer Bulgur <sezerbulgur2@gmail.com>
2024-02-25 11:56:14 +0300

    PlatformAtes ilk duzenlemeler

--- Assets/Scripts/PlatformAtes.cs
+++ Assets/Scripts/PlatformAtes.cs
@@ -4,16 +4,29 @@ using UnityEngine;
 
 public class PlatformAtes : Platform
 {
+    float platformMaxX = 5f;
+    float platformMinX = -5f;
+    float platformMaxY = 2f;
+    float platformMinY = 1.6f;
+    Transform playerTransform;
+
     // Start is called before the first frame update
     void Start()
     {
         
     }
 
-    // Update is called once per frame
+    void OnEnable()
+    {
+        playerTransform = GameObject.FindGameObjectWithTag("Karakter").transform;
+    }
+
     void Update()
     {
-
+        if (playerTransform.position.y - 10 > transform.position.y)
+        {
+            gameObject.SetActive(false);
+        }
     }
 
     void OnCollisionExit2D(Collision2D col)
@@ -23,10 +36,27 @@ public class PlatformAtes : Platform
         {
             // "Karakter" etiketine sahip bir objeye �arpmay� b�rakt���nda �al���r
 
-            Destroy(gameObject);
+            gameObject.SetActive(false);
+            SpawnPlatform();        
         }
     }
 
+    public void SpawnLavaPlatform() 
+    {
 
+        for (int i = 0; i < 3; i++)
+        {
+            GameObject platform = PlatformPool.instance.GetObjectFromSuPlatformPool();
+            if (platform != null)
+            {
+                Vector2 platformVektor = new Vector2();
+                platformVektor.x = Random.Range(platformMinX, platformMaxX);
+                platformVektor.y += Random.Range(playerTransform.position.y, playerTransform.position.y + 5);
+                platform.transform.position = platformVektor;
+                platform.SetActive(true);
+            }
+        }
+        
+    }
 
 }
